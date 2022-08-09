1a9e0bb0a6f0c6653772fc7c8b5f7f195b4b1b4c
archa <104277223+LuckyArchie777@users.noreply.github.com>
2022-08-09 23:12:02 +0300

    added

---
+++ Time counter by triggering/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+    using System.Timers;
+using MyTimer = System.Timers.Timer;
+var timer = new MyTimer();
+{
+timer.Elapsed += (object? _, ElapsedEventArgs e) =>
+Console.WriteLine($"{e.SignalTime}");
+    Console.Clear();
+timer.Interval = 1000;
+timer.Enabled = true;
+Console.ReadLine();
+    }
